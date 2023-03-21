3a038eb5b77a03ad715c63782bdad2b98b86e00d
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2023-03-21 12:35:45 +0000

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -25,8 +25,12 @@ function currentSlide(n) {
 function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
     var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
-    if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
-    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
+    if (n > slides.length) {
+        slideIndex = 1
+    }; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
+    if (n < 1) {
+        slideIndex = slides.length
+    }; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
     }
@@ -39,9 +43,9 @@ function showSlides(n) {
 
 // This code will create close the contact form when the user clicks off of it
 // The first step is to add an event listener for any clicks on the website
-document.addEventListener("click", function(event){
+document.addEventListener("click", function (event) {
     // Here we state that if the click happens on the cancel button OR anywhere that is not the contact form AND the click does not happen on any element with the contact class then call the closeForm() function
-    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")) {
         closeForm()
     }
-}, false )
+}, false)
